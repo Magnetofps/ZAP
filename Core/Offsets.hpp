95a0f0ea95732866abb71384d79a760335607a3e
Magnetofps <165077686+Magnetofps@users.noreply.github.com>
2024-08-27 22:33:32 +0530

    Update Offsets.hpp
    Signed-off-by: Magnetofps <165077686+Magnetofps@users.noreply.github.com>

--- Core/Offsets.hpp
+++ Core/Offsets.hpp
@@ -1,33 +1,33 @@
 #pragma once
 // https://www.unknowncheats.me/forum/apex-legends/319804-apex-legends-reversal-structs-offsets.html
 
-constexpr std::string GAME_VERSION = "v3.0.71.58";
+constexpr std::string GAME_VERSION = "v3.0.78.32";
 constexpr std::string ZAP_VERSION = "ZAP 1.1.2";
 
 // Core
 constexpr long OFF_REGION = 0x140000000;     //[Static]->Region
-constexpr long OFF_LEVEL = 0x17c7160;        //[Miscellaneous]->LevelName
-constexpr long OFF_LOCAL_PLAYER = 0x225a8a8; //[Miscellaneous]->LocalPlayer
-constexpr long OFF_ENTITY_LIST = 0x1eabd08;  //[Miscellaneous]->cl_entitylist
+constexpr long OFF_LEVEL = 0x17ec544;        //[Miscellaneous]->LevelName
+constexpr long OFF_LOCAL_PLAYER = 0x23e91f8; //[Miscellaneous]->LocalPlayer
+constexpr long OFF_ENTITY_LIST = 0x1f15f88;  //[Miscellaneous]->cl_entitylist
 constexpr long OFF_NAME_INDEX = 0x38;        //nameIndex
-constexpr long OFF_NAME_LIST = 0xc7912b0;    //[Miscellaneous]->NameList
-constexpr long OFF_GAMEMODE = 0x22921b0;    // mp_gamemode
+constexpr long OFF_NAME_LIST = 0xd3503c0;    //[Miscellaneous]->NameList
+constexpr long OFF_GAMEMODE = 0x02421250;    // mp_gamemode
 constexpr long OFF_SQUAD_ID = 0x0334;        //m_squadID
-constexpr long OFF_GLOBAL_VARS = 0x17c6c60;  //[Miscellaneous]->GlobalVars
+constexpr long OFF_GLOBAL_VARS = 0x17ec040;  //[Miscellaneous]->GlobalVars
 
 // HUD
-constexpr long OFF_VIEWRENDER = 0x74dd028; //[Miscellaneous]->ViewRenderer
+constexpr long OFF_VIEWRENDER = 0x769b558; //[Miscellaneous]->ViewRenderer
 constexpr long OFF_VIEWMATRIX = 0x11a350;  //[Miscellaneous]->ViewMatrix
 
 // Buttons
-constexpr long OFF_INATTACK = 0x74de2a0;         //[Buttons]->in_attack
-constexpr long OFF_IN_JUMP = 0x74de3a0;          //[Buttons]->in_jump
-constexpr long OFF_IN_DUCK = 0x74de4a0;          //[Buttons]->in_duck
-constexpr long OFF_IN_FORWARD = 0x74de1f0;       //[Buttons]->in_forward
-constexpr long OFF_TRAVERSAL_START_TIME = 0x2af0; //[RecvTable.DT_LocalPlayerExclusive]->m_traversalStartTime
-constexpr long OFF_TRAVERSAL_PROGRESS = 0x2aec;   //[RecvTable.DT_LocalPlayerExclusive]->m_traversalProgress
-constexpr long OFF_WALL_RUN_START_TIME = 0x3594;  //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunStartTime
-constexpr long OFF_WALL_RUN_CLEAR_TIME = 0x3598;  //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunClearTime
+constexpr long OFF_INATTACK = 0x0769b658;         //[Buttons]->in_attack
+constexpr long OFF_IN_JUMP = 0x0769b778;          //[Buttons]->in_jump
+constexpr long OFF_IN_DUCK = 0x0769b868;          //[Buttons]->in_duck
+constexpr long OFF_IN_FORWARD = 0x0769b5a8;       //[Buttons]->in_forward
+constexpr long OFF_TRAVERSAL_START_TIME = 0x2b70; //[RecvTable.DT_LocalPlayerExclusive]->m_traversalStartTime
+constexpr long OFF_TRAVERSAL_PROGRESS = 0x2b6c;   //[RecvTable.DT_LocalPlayerExclusive]->m_traversalProgress
+constexpr long OFF_WALL_RUN_START_TIME = 0x3614;  //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunStartTime
+constexpr long OFF_WALL_RUN_CLEAR_TIME = 0x3618;  //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunClearTime
 
 // Player
 constexpr long OFF_HEALTH = 0x0318;             //[RecvTable.DT_Player]->m_iHealth
@@ -43,14 +43,14 @@ constexpr long OFF_ZOOMING = 0x1bd1;            //[RecvTable.DT_Player]->m_bZoom
 constexpr long OFF_TEAM_NUMBER = 0x0328;        //[RecvTable.DT_BaseEntity]->m_iTeamNum
 constexpr long OFF_NAME = 0x0471;               //[RecvTable.DT_BaseEntity]->m_iName
 constexpr long OFF_LIFE_STATE = 0x0680;         //[RecvTable.DT_Player]->m_lifeState
-constexpr long OFF_BLEEDOUT_STATE = 0x26e0;     //[RecvTable.DT_Player]->m_bleedoutState
+constexpr long OFF_BLEEDOUT_STATE = 0x26e0;     //[RecvTable.DT_Player]->m_bleedoutState ?
 constexpr long OFF_LAST_VISIBLE_TIME = 0x1990;  //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3
 constexpr long OFF_LAST_AIMEDAT_TIME = 0x1998;  //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3 + 0x8
 constexpr long OFF_VIEW_ANGLES = 0x2534 - 0x14; //[DataMap.C_Player]-> m_ammoPoolCapacity - 0x14
 constexpr long OFF_PUNCH_ANGLES = 0x2438;       //[DataMap.C_Player]->m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
 constexpr long OFF_YAW = 0x223c - 0x8;          //m_currentFramePlayer.m_ammoPoolCount - 0x8
 constexpr long OFF_MODELNAME = 0x0030;          //m_ModelName
-constexpr long OFF_OBSERVER_LIST = 0x1EADD28;   //observerList: 48 8B 0D ? ? ? ? 48 85 C9 74 ? 48 8B 01 FF ? ? 48 85 C0 74 ? 48 63 4E 38     10.05.24
+constexpr long OFF_OBSERVER_LIST = 0x1f17ee8;   //observerList: 48 8B 0D ? ? ? ? 48 85 C9 74 ? 48 8B 01 FF ? ? 48 85 C0 74 ? 48 63 4E 38     10.05.24
 
 // Weapon
 constexpr long OFF_WEAPON_HANDLE = 0x1934;            //[RecvTable.DT_Player]->m_latestPrimaryWeapons
@@ -62,8 +62,8 @@ constexpr long OFF_CURRENTZOOMFOV = 0x15d0 + 0x00b8;  //m_playerData + m_curZoom
 constexpr long OFF_TARGETZOOMFOV = 0x15d0 + 0x00bc;   //m_playerData + m_targetZoomFOV
 constexpr long OFF_SKIN = 0x0d58;                     //m_nSkin
 constexpr long OFF_TIME_BASE = 0x2088;                //m_currentFramePlayer.timeBase
-constexpr long OFF_WEAPON_DISCARDED = 0x1599;         //[RecvTable.DT_WeaponX]->m_discarded
-constexpr long OFF_VIEWMODELS = 0x2d18;               //m_hViewModels
+constexpr long OFF_WEAPON_DISCARDED = 0x15a9;         //[RecvTable.DT_WeaponX]->m_discarded
+constexpr long OFF_VIEWMODELS = 0x2d98;               //m_hViewModels
 
 // Glow
 constexpr long OFF_HIGHLIGHT_TYPE_SIZE = 0x34;  //
@@ -71,4 +71,4 @@ constexpr long OFF_GLOW_ENABLE = 0x26c;         //Script_Highlight_GetCurrentCon
 constexpr long OFF_GLOW_THROUGH_WALL = 0x26c;   //Script_Highlight_SetVisibilityType
 constexpr long OFF_GLOW_FIX = 0x268;            //
 constexpr long OFF_GLOW_HIGHLIGHT_ID = 0x28C;   //[DT_HighlightSettings].m_highlightServerActiveStates
-constexpr long OFF_GLOW_HIGHLIGHTS = 0xade5c40; //highlightsettings
+constexpr long OFF_GLOW_HIGHLIGHTS = 0xb13c6a0; //highlightsettings
