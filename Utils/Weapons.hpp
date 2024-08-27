4f31c44a9f9435715b7b0a2c481465af5165a3c2
Magnetofps <165077686+Magnetofps@users.noreply.github.com>
2024-08-27 22:25:05 +0530

    Add files via upload
    Signed-off-by: Magnetofps <165077686+Magnetofps@users.noreply.github.com>

--- Utils/Weapons.hpp
+++ Utils/Weapons.hpp
@@ -3,29 +3,29 @@
 namespace WeaponIDs {
   //Weapon Ids
   //Light
-  inline auto P2020 = 111;
+  inline auto P2020 = 112;
   inline auto RE45 = 85;
   inline auto ALTERNATOR = 84;
-  inline auto R99 = 110;
+  inline auto R99 = 111;
   inline auto R301 = 0;
-  inline auto SPITFIRE = 112;
+  inline auto SPITFIRE = 113;
   inline auto G7 = 95;
 
   //Heavy
   inline auto FLATLINE = 94;
   inline auto HEMLOCK = 96;
   inline auto PROWLER = 107;
-  inline auto REPEATER = 117;
+  inline auto REPEATER = 118;
   inline auto RAMPAGE = 6;
-  inline auto CAR = 118;
+  inline auto CAR = 119;
 
   //Energy
   inline auto HAVOC = 91;
   inline auto DEVOTION = 89;
   inline auto LSTAR = 99;
-  inline auto TRIPLETAKE = 113;
-  inline auto VOLT = 116;
-  inline auto NEMESIS = 119;
+  inline auto TRIPLETAKE = 114;
+  inline auto VOLT = 117;
+  inline auto NEMESIS = 120;
 
   //Shotgun
   inline auto MOZAMBIQUE = 102;
@@ -38,12 +38,12 @@ namespace WeaponIDs {
   inline auto SENTINEL = 1;
 
   //Legendary
-  inline auto WINGMAN = 114;
-  inline auto EVA8 = 93;
+  inline auto WINGMAN = 115;
+  inline auto EVA8 = 92;
   inline auto BOCEK = 2;
   inline auto KRABER = 98;
-  inline auto KNIFE = 169;
+  inline auto KNIFE = 176;
 
   //Melee
-  inline auto HANDS = 120;
+  inline auto HANDS = 121;
 };
